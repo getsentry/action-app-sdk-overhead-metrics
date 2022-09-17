b41c83c1f7e10308a8d900a22a4a75cd9df65d91
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-09-17 14:07:41 +0200

    feat: retry startup time collection on high stddev

--- src/test/kotlin/StartupTimeTest.kt
+++ src/test/kotlin/StartupTimeTest.kt
@@ -1,5 +1,6 @@
 import com.google.common.math.Quantiles
 import com.google.common.math.Stats
+import io.appium.java_client.AppiumDriver
 import io.appium.java_client.android.Activity
 import io.appium.java_client.android.AndroidDriver
 import io.appium.java_client.ios.IOSDriver
@@ -9,11 +10,14 @@ import io.kotest.matchers.doubles.shouldBeLessThan
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.abs
 
 @Suppress("UnstableApiUsage")
 class StartupTimeTest : TestBase() {
-    data class Options(val runs: Int, val diffMax: Int?, val diffMin: Int = 1, val stdDevMax: Int = 50)
+    data class Options(
+        val runs: Int, val diffMax: Int?, val diffMin: Int = 1, val stdDevMax: Int = 50, val retries: Int = 5
+    )
 
     private val options get() = baseOptions.startupTimeTest!!
 
@@ -47,14 +51,20 @@ class StartupTimeTest : TestBase() {
             val stats1 = Stats.of(times1)
             printf(
                 "$logAppPrefix launch times (original) | mean: %3.2f ms | stddev: %3.2f | %d values: $times1",
-                app.name, stats1.mean(), stats1.populationStandardDeviation(), times1.size
+                app.name,
+                stats1.mean(),
+                stats1.populationStandardDeviation(),
+                times1.size
             )
 
             val times2 = filterOutliers(measuredTimes[j])
             val stats2 = Stats.of(times2)
             printf(
                 "$logAppPrefix launch times (filtered) | mean: %3.2f ms | stddev: %3.2f | %d values: $times2",
-                app.name, stats2.mean(), stats2.populationStandardDeviation(), times2.size
+                app.name,
+                stats2.mean(),
+                stats2.populationStandardDeviation(),
+                times2.size
             )
 
             if (options.stdDevMax > 0) {
@@ -92,76 +102,112 @@ class StartupTimeTest : TestBase() {
         val measuredTimes = mutableListOf<List<Long>>()
 
         withDriver { driver ->
-            val runs = options.runs
-            for (j in apps.indices) {
-                val app = apps[j]
+            val appStartCounter = AtomicInteger(0) // needed for iOS time collection
+            for (appIndex in apps.indices) {
+                val app = apps[appIndex]
 
-                // sleep before the first test to improve the first run time
-                Thread.sleep(1_000)
+                for (retry in 1..options.retries) {
+                    // sleep before the first test to improve the first run time
+                    Thread.sleep(1_000)
 
-                // clear logcat before test runs
-                if (baseOptions.platform == TestOptions.Platform.Android) {
-                    driver.manage().logs().get("logcat")
-                }
+                    // clear logcat before test runs
+                    if (baseOptions.platform == TestOptions.Platform.Android) {
+                        driver.manage().logs().get("logcat")
+                    }
 
-                val appTimes = mutableListOf<Long>()
-                for (i in 1..runs) {
-                    printf("$logAppPrefix measuring startup times: %d/%d", app.name, i, runs)
+                    val appTimes = collectAppStartupTimes(app, driver, appStartCounter)
+                    printf(
+                        "$logAppPrefix collected %d/%d startup times (try %d/%d)",
+                        app.name,
+                        appTimes.size,
+                        options.runs,
+                        retry,
+                        options.retries
+                    )
+                    appTimes.size.shouldBe(options.runs)
 
-                    // kill the app and sleep before running the next iteration
-                    val startupTime = when (baseOptions.platform) {
-                        TestOptions.Platform.Android -> {
-                            val androidDriver = (driver as AndroidDriver)
-                            // Note: there's also .activateApp() which should be OS independent, but doesn't seem to wait for the activity to start
-                            androidDriver.startActivity(Activity(app.name, app.activity))
-                            androidDriver.terminateApp(app.name)
-
-                            // Originally we used a code that loaded a list of executed Appium commands and used the time
-                            // that the 'startActivity' command took. It seems like this time includes some overhead of the
-                            // Appium controller because the times were about 900 ms, while the time reported in logcat
-                            // was `ActivityManager: Displayed io.../.MainActivity: +276ms` or `... +1s40ms`
-                            //   val times = driver.events.commands.filter { it.name == "startActivity" } .map { it.endTimestamp - it.startTimestamp }
-                            //   val offset = j * runs
-                            //   times.subList(offset, offset + runs)
-                            val logEntries = driver.manage().logs().get("logcat")
-                            val regex = Regex("Displayed ${app.name}/\\.${app.activity}: \\+(?:([0-9]+)s)?([0-9]+)ms")
-                            val times = logEntries.mapNotNull {
-                                val groups = regex.find(it.message)?.groupValues
-                                if (groups == null) {
-                                    null
-                                } else {
-                                    printf("$logAppPrefix logcat entry processed: %s", app.name, it.message)
-                                    val seconds = if (groups[1].isEmpty()) 0 else groups[1].toLong()
-                                    seconds * 1000 + groups[2].toLong()
-                                }
+                    if (options.stdDevMax > 0) {
+                        val stdDev = Stats.of(filterOutliers(appTimes)).populationStandardDeviation()
+                        if (stdDev > options.stdDevMax.toDouble()) {
+                            printf(
+                                "$logAppPrefix stddev on the filtered startup-time list is too high: %.2f, expected %.2f at most.",
+                                app.name,
+                                stdDev,
+                                options.stdDevMax.toDouble()
+                            )
+                            if (retry < options.retries) {
+                                printf(
+                                    "$logAppPrefix retrying startup time collection: %d/%d",
+                                    app.name,
+                                    retry + 1,
+                                    options.retries
+                                )
+                                continue
                             }
-                            times.shouldHaveSize(1)
-                            times.first()
-                        }
-
-                        TestOptions.Platform.IOS -> {
-                            val iosDriver = (driver as IOSDriver)
-                            iosDriver.activateApp(app.name)
-                            iosDriver.terminateApp(app.name)
-
-                            val times = driver.events.commands.filter { it.name == "activateApp" }
-                                .map { it.endTimestamp - it.startTimestamp }
-                            times.shouldHaveSize(j * runs + i)
-                            times.last()
                         }
                     }
-                    appTimes.add(startupTime)
 
-                    // sleep before the next test run
-                    Thread.sleep(sleepTimeMs)
+                    measuredTimes.add(appTimes)
+                    break
                 }
-
-                printf("$logAppPrefix collected %d/%d startup times", app.name, appTimes.size, runs)
-                appTimes.size.shouldBe(runs)
-                measuredTimes.add(appTimes)
             }
         }
 
         return measuredTimes
+    }
+
+    private fun collectAppStartupTimes(app: AppInfo, driver: AppiumDriver, counter: AtomicInteger): MutableList<Long> {
+        val appTimes = mutableListOf<Long>()
+        for (i in 1..options.runs) {
+            printf("$logAppPrefix measuring startup times: %d/%d", app.name, i, options.runs)
+
+            // kill the app and sleep before running the next iteration
+            val startupTime = when (baseOptions.platform) {
+                TestOptions.Platform.Android -> {
+                    val androidDriver = (driver as AndroidDriver)
+                    // Note: there's also .activateApp() which should be OS independent, but doesn't seem to wait for the activity to start
+                    androidDriver.startActivity(Activity(app.name, app.activity))
+                    androidDriver.terminateApp(app.name)
+
+                    // Originally we used a code that loaded a list of executed Appium commands and used the time
+                    // that the 'startActivity' command took. It seems like this time includes some overhead of the
+                    // Appium controller because the times were about 900 ms, while the time reported in logcat
+                    // was `ActivityManager: Displayed io.../.MainActivity: +276ms` or `... +1s40ms`
+                    //   val times = driver.events.commands.filter { it.name == "startActivity" } .map { it.endTimestamp - it.startTimestamp }
+                    //   val offset = j * runs
+                    //   times.subList(offset, offset + runs)
+                    val logEntries = driver.manage().logs().get("logcat")
+                    val regex = Regex("Displayed ${app.name}/\\.${app.activity}: \\+(?:([0-9]+)s)?([0-9]+)ms")
+                    val times = logEntries.mapNotNull {
+                        val groups = regex.find(it.message)?.groupValues
+                        if (groups == null) {
+                            null
+                        } else {
+                            printf("$logAppPrefix logcat entry processed: %s", app.name, it.message)
+                            val seconds = if (groups[1].isEmpty()) 0 else groups[1].toLong()
+                            seconds * 1000 + groups[2].toLong()
+                        }
+                    }
+                    times.shouldHaveSize(1)
+                    times.first()
+                }
+
+                TestOptions.Platform.IOS -> {
+                    val iosDriver = (driver as IOSDriver)
+                    iosDriver.activateApp(app.name)
+                    iosDriver.terminateApp(app.name)
+
+                    val times = driver.events.commands.filter { it.name == "activateApp" }
+                        .map { it.endTimestamp - it.startTimestamp }
+                    times.shouldHaveSize(counter.incrementAndGet())
+                    times.last()
+                }
+            }
+            appTimes.add(startupTime)
+
+            // sleep before the next test run
+            Thread.sleep(sleepTimeMs)
+        }
+        return appTimes
     }
 }
