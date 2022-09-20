c606c05214dbb2c469c6da9823df259afd8d5622
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-09-20 11:54:14 +0200

    fix: specify working directory for git commands

--- src/main/kotlin/Git.kt
+++ src/main/kotlin/Git.kt
@@ -1,4 +1,5 @@
 import java.io.BufferedReader
+import java.io.File
 import java.io.InputStreamReader
 
 class Git {
@@ -10,27 +11,32 @@ class Git {
             if (baseRef.isNullOrEmpty()) defaultBranch else baseRef
         }
 
+        private val workingDirectory =
+            if (System.getenv().containsKey("CI")) File(System.getenv("GITHUB_WORKSPACE")!!) else null
+
         private val defaultBranch by lazy {
             Regex("HEAD branch: *(.*)").find(executeCommand("git remote show origin"))!!.groupValues[1]
         }
 
         val hash by lazy {
             var gitHash = executeCommand("git rev-parse HEAD")
-            if (Runtime.getRuntime().exec("git diff --quiet HEAD").waitFor() != 0) {
+            if (exec("git diff --quiet HEAD").waitFor() != 0) {
                 gitHash += "+dirty"
             }
             gitHash
         }
-    }
-}
 
-fun executeCommand(command: String): String {
-    val process = Runtime.getRuntime().exec(command)
-    val stdOut = BufferedReader(InputStreamReader(process.inputStream))
-    val stdErr = BufferedReader(InputStreamReader(process.errorStream))
-    if (process.waitFor() == 0) {
-        return stdOut.readText().trim()
-    } else {
-        throw Exception("Command '$command' exited with code ${process.waitFor()}\n${stdErr.readText().trim()}")
+        private fun exec(command: String) = Runtime.getRuntime().exec(command, null, workingDirectory)
+
+        private fun executeCommand(command: String): String {
+            val process = exec(command)
+            val stdOut = BufferedReader(InputStreamReader(process.inputStream))
+            val stdErr = BufferedReader(InputStreamReader(process.errorStream))
+            if (process.waitFor() == 0) {
+                return stdOut.readText().trim()
+            } else {
+                throw Exception("Command '$command' exited with code ${process.waitFor()}\n${stdErr.readText().trim()}")
+            }
+        }
     }
 }
